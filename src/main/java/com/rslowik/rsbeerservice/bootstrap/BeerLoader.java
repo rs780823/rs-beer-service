40691e0898aa7d5f142fefdf02d5e814c77313b8
Rafal <rafal_slowik@o2.pl>
2020-10-08 20:44:26 +0200

    Adding entities loader

---
+++ src/main/java/com/rslowik/rsbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,47 @@
+package com.rslowik.rsbeerservice.bootstrap;
+
+import com.rslowik.rsbeerservice.domain.Beer;
+import com.rslowik.rsbeerservice.repositories.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository repository;
+
+    public BeerLoader(BeerRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if (repository.count() == 0) {
+            repository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000001L)
+                    .price(new BigDecimal("12.95"))
+                    .build()
+            );
+
+            repository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000002L)
+                    .price(new BigDecimal("11.95"))
+                    .build()
+            );
+        }
+    }
+}
